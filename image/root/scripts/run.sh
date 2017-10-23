b7c3534800c9737fd5d124ff60a9de624ab94ced
Emory Merryman <emory.merryman@gmail.com>
2017-10-23 14:19:15 +0000

    add a fake plugin

--- image/root/scripts/run.sh
+++ image/root/scripts/run.sh
@@ -6,5 +6,7 @@ apk update &&
     adduser -D user &&
     cp /opt/docker/etc/sudo.txt /etc/sudoers.d/user &&
     chmod 0444 /etc/sudoers.d/user &&
+    mkdir /etc/docker &&
+    mkdir /etc/docker/plugins &&
     cp /opt/docker/plugins/myplugin.json /etc/docker/plugins &&
     rm -rf /var/cache/apk/*
