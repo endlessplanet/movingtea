870fd8a56e8b6a510e2aca41622e3b04f8bab403
Emory Merryman <emory.merryman@gmail.com>
2017-10-23 13:29:44 +0000

    add exec file

--- bin/exec.sh
+++ bin/exec.sh
@@ -8,7 +8,7 @@ DIND=$(mktemp) &&
             docker network rm $(cat ${NETW}) &&
             rm -f ${DIND} ${NETW}
     }
-    docker network create > ${NETW} &&
+    docker network create $(uuidgen) > ${NETW} &&
     docker container run --cidfile ${DIND} --detach --privileged endlessplanet/movingtea:$(git rev-parse --verify HEAD) &&
     docker network connect --alias docker $(cat ${NETW}) $(cat ${DIND}) &&
     docker container run --interactive --tty --rm --env DOCKER_HOST=tcp://docker:2376 --network ${NETW} docker:17.10.0 sh
