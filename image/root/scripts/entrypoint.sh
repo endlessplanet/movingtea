363edc110a3ccbc5030604527a0105573e34e131
Emory Merryman <emory.merryman@gmail.com>
2017-10-23 13:34:00 +0000

    add sudo

--- image/root/scripts/entrypoint.sh
+++ image/root/scripts/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-dockerd-entrypoint.sh "${@}"
+sudo dockerd-entrypoint.sh "${@}"
--- image/root/scripts/run.sh
+++ image/root/scripts/run.sh
@@ -3,4 +3,7 @@
 apk update &&
     apk upgrade &&
     apk add --no-cache sudo &&
+    adduser -D user &&
+    cp /opt/docker/etc/sudo.txt /etc/sudoers.d/user &&
+    chmod 0444 /etc/sudoers.d/user &&
     rm -rf /var/cache/apk/*
